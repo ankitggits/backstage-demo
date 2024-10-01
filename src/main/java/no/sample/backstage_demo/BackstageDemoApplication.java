dc07c1755b950254cf452b35f1a5d8223f0045c5
ankit <ankit.singh@storebrand.no>
2024-10-01 12:03:59 +0200

    initial commit

--- src/main/java/no/sample/backstage_demo/BackstageDemoApplication.java
+++ src/main/java/no/sample/backstage_demo/BackstageDemoApplication.java
@@ -2,12 +2,20 @@ package no.sample.backstage_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BackstageDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackstageDemoApplication.class, args);
+	}
+
+	@GetMapping("greet")
+	public String hello() {
+		return "Hello World";
 	}
 
 }
