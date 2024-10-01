a7f68529c0db76ea4648c7dcb335d6b279154ffb
ankit <ankit.singh@storebrand.no>
2024-10-01 10:47:03 +0200

    initial commit

---
+++ src/main/java/no/sample/backstage_demo/BackstageDemoApplication.java
@@ -0,0 +1,13 @@
+package no.sample.backstage_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/sample/backstage_demo/BackstageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.sample.backstage_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
